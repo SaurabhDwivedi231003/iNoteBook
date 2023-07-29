0d5accd740e7d37d84042939d896ad37d215fba0
SAURABH <saurabhdwivedi2310@gmail.com>
2023-07-29 15:55:53 +0530

    Added update Note feature and Bootstrap MODAL

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -7,8 +7,11 @@ const NoteState = (props) => {
   const [notes, setNotes] = useState(notesInitial)
 
   // Get all Notes
+  
   const getNotes = async () => {
     // API Call 
+      // eslint-disable-next-line 
+
     const response = await fetch(`${host}/api/notes/fetchallnotes`, {
       method: 'GET',
       headers: {
@@ -49,12 +52,25 @@ const NoteState = (props) => {
   }
 
   // Delete a Note
-  const deleteNote = (id) => {
-    // TODO: API Call
+  const deleteNote = async (id) => {
+    // API Call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiZmYwMTNkMTY1ZDFmOWZmYTJhZjJlIn0sImlhdCI6MTY5MDMwMTQyMn0._NvHEg3VqXZkShfi8iBGNcqr3aet_knHRQewk6hB5RI" 
+      }
+    });
+    const json = response.json();
+    console.log(json);
+    
+
     console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
+
+
   // Edit a Note
   const editNote = async (id, title, description, tag) => {
     // API Call 
